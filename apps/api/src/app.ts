7c81212ce79d464be151714fe38ec0b4e6dc88f8
Julian Amigo <julamifra@gmail.com>
2026-06-27 23:19:58 +0200

    add cors mw

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -9,6 +9,19 @@ import { notFoundHandler } from "./middleware/not-found";
 export const createApp = () => {
   const app = express();
 
+  app.use((_req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+    if (_req.method === "OPTIONS") {
+      res.sendStatus(204);
+      return;
+    }
+
+    next();
+  });
+
   app.use(express.json());
 
   app.use("/api", healthRouter);

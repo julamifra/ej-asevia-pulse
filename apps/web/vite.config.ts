c9f2f79900c50bcc82a71553b0b5ee66238aeb70
Julian Amigo <julamifra@gmail.com>
2026-06-28 17:45:44 +0200

    add ci/cd pipeline

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,10 +1,6 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: "jsdom",
-    setupFiles: "./src/test/setup.ts"
-  }
+  plugins: [react()]
 });
